08938e79a440d04b0c43fa04b96a8ea352f994b1
Paige Warren <Paigewarren1@icloud.com>
2026-03-12 14:46:03 +0000

    Added boot strap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <h1 className="text-primary">Welcome to My App</h1>
+        <button className="btn btn-success">Click Me</button>
+      </div>
+      <footer>
+        This project was coded by Paige Warren and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Paige9-web/react-weather-app-04"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://app.netlify.com/projects/react-weather-app-pw/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
