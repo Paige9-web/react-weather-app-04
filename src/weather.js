c70fa62267c1a70237fc018b99195f53cab82de1
Paige Warren <Paigewarren1@icloud.com>
2026-03-13 16:26:01 +0000

    Added css

--- src/weather.js
+++ src/weather.js
@@ -12,22 +12,22 @@ export default function Weather() {
 
       <h1>Current location</h1>
 
-      <ul className="list-unstyled">
+      <ul className="default-weather">
         <li>Wednesday 7:00</li>
         <li>Mostly cloudy</li>
       </ul>
 
       <div className="row">
         <div className="col-6">
-          6°C
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly cloudy"
           />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
-
         <div className="col-6">
-          <ul className="list-unstyled">
+          <ul className="additional-info">
             <li>Precipitation: 15%</li>
             <li>Humidity: 5%</li>
             <li>Wind: 13km</li>
