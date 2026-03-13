437689c2794567ea990f1f798f2ee191fa60c06c
Paige Warren <Paigewarren1@icloud.com>
2026-03-13 16:34:02 +0000

    removed empty h1 heading

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="text-primary"></h1>
         <Weather />
       </div>
       <footer>
