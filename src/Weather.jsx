7aea0e5c74fd84ee423a29234e34ba2013a61f82
Paige Warren <Paigewarren1@icloud.com>
2026-03-15 19:21:59 +0000

    Update Weather.jsx

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherTemp from "./WeatherTemperature";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weather() {
   const [city, setCity] = useState("");
